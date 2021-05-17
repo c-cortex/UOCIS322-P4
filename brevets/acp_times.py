7a1e03aa0f5d8d1e85796cf9b8c77af6945fc9e9
Chris Cortes <cortes.d.chris@gmail.com>
2021-05-16 22:03:00 -0700

    Edit to acp_times.py

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -26,7 +26,38 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    # if else statements to see which range the control distance falls in
+    # within each range, more if else statemnts to determine the max time for controls exceding brevet length
+    if control_dist_km == 0:
+        time = 0
+    elif control_dist_km <= 200:
+        time = control_dist_km/34
+    elif control_dist_km <= 400:
+        if brevet_dist_km == 200:
+            time = 200/34
+        elif brevet_dist_km == 300:
+            time = (200/34) + (100/32)
+        else:
+            time = (200/34) + ((control_dist_km-200)/32)
+    elif control_dist_km <= 600:
+        if brevet_dist_km == 400:
+            time = (200/34) + (200/32)
+        else:
+            time = (200/34) + (200/32) + ((control_dist_km-400)/30)
+    elif control_dist_km <= 1000:
+        if brevet_dist_km == 600:
+            time = (200/34) + (200/32) + (200/30)
+        else:
+            time = (200/34) + (200/32) + (200/30) + ((control_dist_km- 600)/28)
+    elif control_dist_km <= 1300:
+        if brevet_dist_km == 1000:
+            time = (200/34) + (200/32) + (200/30) + (400/28)
+        else:
+            time = (200/34) + (200/32) + (200/30) + (400/28) + ((control_dist_km-1000)/26)
+    # round to minute because there are no seconds in calculator
+    min = round(time*60)
+    # adjusts start time arrow by the minutes found in if else statements
+    return brevet_start_time.shift(minutes=min)
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -41,4 +72,35 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    # if else statements to see which range the control distance falls in
+    # within each range, more if else statemnts to determine the max time for controls exceding brevet length
+    if control_dist_km == 0:
+        time = 1
+    elif control_dist_km <= 200:
+        time = control_dist_km/15
+    elif control_dist_km <= 400:
+        if brevet_dist_km == 200:
+            time = 13.5
+        elif brevet_dist_km == 300:
+            time = 20
+        else:
+            time = control_dist_km/15
+    elif control_dist_km <= 600:
+        if brevet_dist_km == 400:
+            time = 27
+        else:
+            time = control_dist_km/15
+    elif control_dist_km <= 1000:
+        if brevet_dist_km == 600:
+            time = 40
+        else:
+            time = (600/15) + ((control_dist_km-600)/11.428)
+    elif control_dist_km <= 1300:
+        if brevet_dist_km == 1000:
+            time = 75
+        else:
+            time = (600/15) + (400/11.428) + ((control_dist_km-1000)/13.333)
+    # round to minute because there are no seconds in calculator
+    min = round(time*60)
+    # adjusts start time arrow by the minutes found in if else statements
+    return brevet_start_time.shift(minutes=min)
